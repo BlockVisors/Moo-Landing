28092c5feb64338f9d237c07676d389fd1b35b39
MooFinance <moofinance2020@gmail.com>
2020-10-08 13:23:55 -0400

    update ccsss

--- next.config.js
+++ next.config.js
@@ -1,24 +1,33 @@
-const path = require('path')
-const withCss = require('@zeit/next-css')
-const withSass = require('@zeit/next-sass')
+// const path = require('path')
+// const withCss = require('@zeit/next-css')
+// const withSass = require('@zeit/next-sass')
+// const withImages = require('next-images')
+// module.exports = withImages(withSass(withCss({
+//         webpack: (config, { isServer }) => {
+//           if (isServer) {
+//             config.module.rules.push({
+//               test: /\.(png|jpg|gif|svg|eot|ttf|woff|woff2)$/,
+//               use: {
+//                   loader: 'url-loader',
+//                   options: {
+//                       limit: 1000000,
+//                       name: '[name].[ext]'
+//                   }
+//               }
+//           })
+//           }
+//           return config
+//         },
+//         cssLoaderOptions: {
+//           url: false
+//         }
+//       })))
+
+// next.config.js
 const withImages = require('next-images')
-module.exports = withImages(withSass(withCss({
-        webpack: (config, { isServer }) => {
-          if (isServer) {
-            config.module.rules.push({
-              test: /\.(png|jpg|gif|svg|eot|ttf|woff|woff2)$/,
-              use: {
-                  loader: 'url-loader',
-                  options: {
-                      limit: 1000000,
-                      name: '[name].[ext]'
-                  }
-              }
-          })
-          }
-          return config
-        },
-        cssLoaderOptions: {
-          url: false
-        }
-      })))
+module.exports = withImages({
+  fileExtensions: ["jpg", "jpeg", "png", "gif"],
+  webpack(config, options) {
+    return config
+  }
+})
